c8db81a6e39922b6a73c200d3557efd60f53c649
Jindřich <prec.jindrich@gmail.com>
2020-07-29 21:10:35 +0200

    working on modal

--- src/App.js
+++ src/App.js
@@ -5,10 +5,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className="modal">
+          <div className="modal__header">
+            <h2 className="modal__title">Header Settings</h2>
+            <span className="modal__label">Size</span>
+            <div className="segment-control">
+              <label>
+                <input type="radio" name="size" value="s" />
+                <span>S</span>
+              </label>
+              <label>
+                <input type="radio" name="size" value="m" checked />
+                <span>M</span>
+              </label>
+              <label>
+                <input type="radio" name="size" value="l" />
+                <span>L</span>
+              </label>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
+
