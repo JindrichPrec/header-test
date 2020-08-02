540d2db49aee86a6498636384bbbaaad364a4e59
Jindřich <prec.jindrich@gmail.com>
2020-08-02 20:03:16 +0200

    implement carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import './Carousel.sass'
+import './Carousel.sass';
 
-export function Carousel({children, duration, }) {
-    const items = [1,2,3,4,5];
+export function Carousel({slideInterval}) {
+    let items = [1,2,3,4,5];
+    let duration = slideInterval !== undefined && slideInterval >= 2000 ? slideInterval : 2000
+
+    const [x, setX] = useState(0);
+
+    const goLeft = () => {
+        x === 0 ? setX(-100 * (items.length - 1)) : setX(x + 100);
+    };
+    const goRight = () => {
+        x === -100 * (items.length - 1) ? setX(0) : setX(x - 100);
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          goRight();
+        }, duration);
+        return () => clearInterval(interval);
+      });
+
     return (
         <div className="carousel">
-            {items.map((item) =>
-                <div className="carousel__slide" key={item.toString()}>
+            {items.map((item, index) =>
+                <div className="carousel__slide" key={index} style={{ transform: `translateX(${x}%)`}} >
                     <span>{item}</span>
                 </div>
             )}
-            <button className="carousel__btn carousel__btn--r">Right</button>
-            <button className="carousel__btn carousel__btn--l">Left</button>
+            <button className="carousel__btn carousel__btn--r" onClick={goRight}>Right</button>
+            <button className="carousel__btn carousel__btn--l" onClick={goLeft}>Left</button>
         </div>
     )    
 }
