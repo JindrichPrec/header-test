7c41e9eca6d315f9c80ec9b1bdb2a91a234cce47
Jindřich <prec.jindrich@gmail.com>
2020-08-02 20:04:02 +0200

    fix popup behavior

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -2,18 +2,26 @@ import React, { useEffect, useRef } from 'react';
 
 import './Popup.sass';
 
+function isChildOf(parent, child) {
+    while (child !== null && child !== parent) {
+      child = child.parentElement;
+    }
+    return child === parent;
+}
+
 export function Popup({isOpen, children, onClose}) {
     const containerRef = useRef();
     useEffect(() => {
         if (isOpen) {
-            const onPointerDown = (e) => {
-                if (e.target !== containerRef.current) {
+            const onClick = (e) => {
+                if (!isChildOf(containerRef.current, e.target)) {
                     onClose();
+                    e.preventDefault();
                 }
             };
-            window.addEventListener('pointerdown', onPointerDown);
+            window.addEventListener('click', onClick);
             return () => {
-                window.removeEventListener('pointerdown', onPointerDown);
+                window.removeEventListener('click', onClick);
             };
         }
     }, [isOpen, onClose]);
